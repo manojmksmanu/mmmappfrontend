703fd3d527a6b86b8999b36de4f4f08653524c43
manojmksmanu <manoj2022019@gmail.com>
2024-10-17 21:05:15 +0530

    every thing up to date

--- src/misc/fireBaseConfig/fireBaseConfig.ts
+++ src/misc/fireBaseConfig/fireBaseConfig.ts
@@ -11,16 +11,6 @@ const firebaseConfig = {
   measurementId: "G-WP8JDP714Y",
   databaseURL: "https://mmmapp-7f726-default-rtdb.firebaseio.com/", // Add this line
 };
-// const firebaseConfig = {
-//   apiKey: "AIzaSyAybI_dJ1ZTy6XmJodG8qZ_9fQ-cWAVjnM",
-//   authDomain: "reactnative-a9cd0.firebaseapp.com",
-//   projectId: "reactnative-a9cd0",
-//   storageBucket: "reactnative-a9cd0.appspot.com",
-//   messagingSenderId: "1077116040460",
-//   appId: "1:1077116040460:web:cc36eccf776193c0848529",
-//   measurementId: "G-4C5V3PD6Q4",
-//   databaseURL: "https://reactnative-a9cd0.firebaseio.com",
-// };
 
 // Initialize Firebase app if it hasn't been initialized already
 const app =
