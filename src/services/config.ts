9d9a89bfb0068853f6300323849771c2f6941d74
manojmksmanu <manoj2022019@gmail.com>
2024-12-07 11:09:52 +0530

    backend url changed

--- src/services/config.ts
+++ src/services/config.ts
@@ -3,5 +3,5 @@ import { Platform } from "react-native";
 // Function to return the base URL based on the environment
 const LOCAL_IP = "192.168.1.3"; // Replace with your machine's local IP address
 
-export const BASE_URL = "http://10.0.2.2:5000";
-// export const BASE_URL = "https://mmmappbackend-yrsy.onrender.com";
+// export const BASE_URL = "http://10.0.2.2:5000";
+export const BASE_URL = "https://mmmappbackend-yrsy.onrender.com";
